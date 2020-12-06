a4dde13934cc5c402b9922f44289d8cf84d1fd2c
Kévin Gomez <kevin.gomez@voiapp.io>
2020-12-06 03:00:23 +0100

    Display incidents in the main opsgenie page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AlertsPage from './components/AlertsPage/AlertsPage';
+import MainPage from './components/MainPage/MainPage';
 import { rootRouteRef } from './plugin';
 
 export const Router = () => (
   <Routes>
-    <Route path={`/${rootRouteRef.path}`} element={<AlertsPage />} />
+    <Route path={`/${rootRouteRef.path}`} element={<MainPage />} />
   </Routes>
 );

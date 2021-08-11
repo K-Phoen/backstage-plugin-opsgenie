f838f00f83cd32fed90ea1d0122c3479f3ec6748
Kévin Gomez <kevin.gomez@voiapp.io>
2021-08-11 22:12:49 +0200

    Quarterly incidents by responders

--- src/components/Analytics/utils.tsx
+++ src/components/Analytics/utils.tsx
@@ -44,16 +44,3 @@ export const exportGraph = (domNodeId: string) => {
             fileDownload(blob, domNodeId + '.png');
         });
 }
-
-export const stringToColour = (str: string) => {
-    var hash = 0;
-    for (var i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-        var value = (hash >> (i * 8)) & 0xFF;
-        colour += ('00' + value.toString(16)).substr(-2);
-    }
-    return colour;
-}

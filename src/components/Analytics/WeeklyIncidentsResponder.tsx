8e36eb53ce39c77eb28ad78b1be7886a3d5e1860
Kévin Gomez <kevin.gomez@voiapp.io>
2021-08-13 00:07:33 +0200

    Little code cleanup

--- src/components/Analytics/WeeklyIncidentsResponder.tsx
+++ src/components/Analytics/WeeklyIncidentsResponder.tsx
@@ -1,39 +1,20 @@
 import React from 'react';
-import {
-    ComposedChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip, Legend
-} from 'recharts';
 import { InfoCard } from '@backstage/core-components';
 import { useApi } from '@backstage/core-plugin-api';
-import stc from 'string-to-color';
 import { analyticsApiRef, Context } from '../../analytics';
 import { SaveAction } from './SaveAction';
+import { PeriodByResponderGraph } from './PeriodByResponderGraph';
 
-const ResponderGraph = ({context}: {context: Context}) => {
+export const WeeklyIncidentsResponders = ({context}: {context: Context}) => {
+    const graphId = "weekly-incidents-responders";
     const analyticsApi = useApi(analyticsApiRef);
     const data = analyticsApi.incidentsByWeekAndResponder(context);
 
     return (
-        <div id="weekly-incidents-responders" style={{ width: '100%', height: 300, paddingTop: '1.2rem', paddingRight: '1.2rem' }}>
-            <ResponsiveContainer>
-                <ComposedChart data={data!.dataPoints}>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="period" />
-                    <YAxis />
-                    {data!.responders.map(responder => (
-                        <Bar dataKey={responder} fill={stc(responder)} stackId="a" barSize={30}  key={responder} />
-                    ))}
-                    <Tooltip />
-                    <Legend />
-                </ComposedChart>
-            </ResponsiveContainer>
+        <InfoCard title="Incidents by week and responder" action={<SaveAction targetRef={graphId} />}>
+        <div id={graphId} style={{ width: '100%', height: 300, paddingTop: '1.2rem', paddingRight: '1.2rem' }}>
+            <PeriodByResponderGraph data={data!} />
         </div>
-    );
-};
-
-export const WeeklyIncidentsResponders = ({context}: {context: Context}) => {
-    return (
-        <InfoCard title="Incidents by week and responder" action={<SaveAction targetRef="weekly-incidents-responders" />}>
-            <ResponderGraph context={context} />
-        </InfoCard>
+    </InfoCard>
     );
 };

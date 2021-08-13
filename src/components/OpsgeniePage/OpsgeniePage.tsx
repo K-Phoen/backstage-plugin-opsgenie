4cd2e5dacb3266972e732217541fc0b858f26469
Kévin Gomez <kevin.gomez@voiapp.io>
2021-08-13 13:00:30 +0200

    Setup tab-based routing for main page

--- src/components/OpsgeniePage/OpsgeniePage.tsx
+++ src/components/OpsgeniePage/OpsgeniePage.tsx
@@ -2,36 +2,24 @@ import React from 'react';
 import { AlertsList } from '../AlertsTable';
 import { IncidentsList } from '../IncidentsTable';
 import { OnCallList } from '../OnCallList';
-import { Page, Header, Content, Tabs } from '@backstage/core-components';
 import { Analytics } from '../Analytics';
+import { Layout } from './Layout';
 
 export const OpsgeniePage = () => {
     return (
-        <Page themeId="tool">
-            <Header title="Opsgenie" type="tool" />
-
-            <Content>
-                <Tabs
-                    tabs={[
-                        {
-                            label: 'Who is on-call',
-                            content: <OnCallList />,
-                        },
-                        {
-                            label: 'Alerts',
-                            content: <AlertsList />,
-                        },
-                        {
-                            label: 'Incidents',
-                            content: <IncidentsList />,
-                        },
-                        {
-                            label: 'Analytics',
-                            content: <Analytics />,
-                        },
-                    ]}
-                />
-            </Content>
-        </Page>
+        <Layout>
+            <Layout.Route path="who-is-on-call" title="Who is on-call">
+                <OnCallList />
+            </Layout.Route>
+            <Layout.Route path="alerts" title="Alerts">
+                <AlertsList />
+            </Layout.Route>
+            <Layout.Route path="incidents" title="Incidents">
+                <IncidentsList />
+            </Layout.Route>
+            <Layout.Route path="analytics" title="Analytics">
+                <Analytics />
+            </Layout.Route>
+        </Layout>
     );
 };

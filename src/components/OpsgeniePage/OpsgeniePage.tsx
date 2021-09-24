a444c836eb3b3463a440dfac1cf1d7d7727a1a83
Phil Kuang <pkuang@factset.com>
2021-09-24 10:29:06 -0400

    feat(OpsgeniePage): support custom Opsgenie page

--- src/components/OpsgeniePage/OpsgeniePage.tsx
+++ src/components/OpsgeniePage/OpsgeniePage.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import { AlertsList } from '../AlertsTable';
-import { IncidentsList } from '../IncidentsTable';
-import { OnCallList } from '../OnCallList';
-import { Analytics } from '../Analytics';
-import { Layout } from './Layout';
+import { useOutlet } from 'react-router';
+import { DefaultOpsgeniePage } from './DefaultOpsgeniePage';
 
 export const OpsgeniePage = () => {
-    return (
-        <Layout>
-            <Layout.Route path="who-is-on-call" title="Who is on-call">
-                <OnCallList />
-            </Layout.Route>
-            <Layout.Route path="alerts" title="Alerts">
-                <AlertsList />
-            </Layout.Route>
-            <Layout.Route path="incidents" title="Incidents">
-                <IncidentsList />
-            </Layout.Route>
-            <Layout.Route path="analytics" title="Analytics">
-                <Analytics />
-            </Layout.Route>
-        </Layout>
-    );
+  const outlet = useOutlet();
+
+  return outlet || <DefaultOpsgeniePage />;
 };
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export * from './extensions';
 export { opsGeniePlugin } from './plugin';
 export * from './api';
 export * from './integration';
+export { Layout } from './components/OpsgeniePage';
+export { AlertsList } from './components/AlertsTable';
+export { IncidentsList } from './components/IncidentsTable';
+export { OnCallList } from './components/OnCallList';
+export { Analytics } from './components/Analytics';

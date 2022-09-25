0889b0fec11219579ebe0fdd904ab2d9982147da
Kévin Gomez <kevin.gomez@voiapp.io>
2022-09-25 19:00:36 +0200

    Bump Backstage dependencies

--- src/components/Analytics/WeeklyImpactResponder.tsx
+++ src/components/Analytics/WeeklyImpactResponder.tsx
@@ -6,7 +6,51 @@ import { SaveAction } from './SaveAction';
 import { colorForString } from './utils';
 import { Bar, CartesianGrid, ComposedChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 import moment from 'moment';
-import { FilterZeroTooltip } from './FilterZeroTooltip';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    tooltip: {
+        backgroundColor: theme.palette.background.default,
+        paddingLeft: '1rem',
+        paddingRight: '1rem',
+        paddingBottom: '0.5rem',
+        paddingTop: '0.5rem',
+        borderRadius: '5px',
+        border: '1px solid white',
+        borderColor: theme.palette.grey[300],
+    },
+}));
+
+const formatDuration = (value: number) => value === 0 ? '0 min' : moment.duration(value, 'minutes').humanize();
+
+const DurationTooltip = (props: any) => {
+    const classes = useStyles();
+
+    if (!props.active || !props.payload || !props.payload.length) {
+        return null;
+    }
+
+    const filteredPayload = props.payload.filter((item: any) => item.payload[item.dataKey] !== 0);
+    if (!filteredPayload.length) {
+        return null;
+    }
+
+    const items = filteredPayload.map((row: any) => {
+        const value = formatDuration(row.payload[row.dataKey]);
+
+        return (
+            <p key={row.dataKey} style={{ color: row.fill }}>
+                {`${row.name}: ${value}`}
+            </p>
+        );
+    });
+
+    return (
+        <div className={classes.tooltip}>
+            {items}
+        </div>
+    );
+};
 
 export const WeeklyImpactResponders = ({ context }: { context: Context }) => {
     const graphId = "weekly-impact-responders";
@@ -27,10 +71,7 @@ export const WeeklyImpactResponders = ({ context }: { context: Context }) => {
                             <Bar dataKey={responder} fill={colorForString(responder)} stackId="a" barSize={30} key={responder} />
                         ))}
 
-                        <Tooltip
-                            formatter={(value: number, name: string) => [value === 0 ? '0 min' : moment.duration(value, 'minutes').humanize(), name]}
-                            content={<FilterZeroTooltip />}
-                        />
+                        <Tooltip content={<DurationTooltip />} />
                         <Legend />
                     </ComposedChart>
                 </ResponsiveContainer>
